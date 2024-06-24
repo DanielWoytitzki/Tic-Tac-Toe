0dab4323e8828e06d0061c0962613835415c3ea1
Daniel Woytitzki <danielwoytitzki@icloud.com>
2024-06-24 12:53:01 +0200

    changed design and added animation to circle and cross

--- script.js
+++ script.js
@@ -21,9 +21,9 @@ function render() {
             let value = fields[index];
             let displayValue = '';
             if (value === 'circle') {
-                displayValue = 'O';
+                displayValue = generateCircleSVG();
             } else if (value === 'cross') {
-                displayValue = 'X';
+                displayValue = generateCrossSVG();
             }
             html += `<td>${displayValue}</td>`;
         }
@@ -32,4 +32,43 @@ function render() {
     
     html += '</table>';
     content.innerHTML = html;
+}
+
+function generateCircleSVG() {
+    return `
+        <svg width="70" height="70" viewBox="0 0 70 70" xmlns="http://www.w3.org/2000/svg">
+            <circle cx="35" cy="35" r="30" stroke="#00B0EF" stroke-width="5" fill="none">
+                <animate 
+                    attributeName="stroke-dasharray"
+                    from="0, 188.4"
+                    to="188.4, 188.4"
+                    dur="500ms"
+                    fill="freeze" />
+            </circle>
+        </svg>
+    `;
+}
+
+function generateCrossSVG() {
+    return `
+        <svg width="70" height="70" viewBox="0 0 70 70" xmlns="http://www.w3.org/2000/svg">
+            <line x1="10" y1="10" x2="60" y2="60" stroke="#FFC000" stroke-width="5">
+                <animate 
+                    attributeName="stroke-dasharray"
+                    from="0, 70"
+                    to="70, 70"
+                    dur="250ms"
+                    fill="freeze" />
+            </line>
+            <line x1="60" y1="10" x2="10" y2="60" stroke="#FFC000" stroke-width="5">
+                <animate 
+                    attributeName="stroke-dasharray"
+                    from="0, 70"
+                    to="70, 70"
+                    dur="250ms"
+                    fill="freeze"
+                    begin="250ms" />
+            </line>
+        </svg>
+    `;
 }
